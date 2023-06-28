92906d25c391c134ef7322d9cb1b720d0f042d3b
Janis <janis.meister87@gmail.com>
2023-06-28 18:57:32 +0200

    [feature] add key to div

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -72,7 +72,7 @@ export default function Viewer({
             </div>
           );
         } else {
-          return <></>;
+          return <div key={Math.random()}></div>;
         }
       })}
     </div>
