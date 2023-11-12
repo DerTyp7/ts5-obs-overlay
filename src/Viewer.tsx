f0761c40b72cc33f6bfceb7ea0c8cba26eff8060
Janis <janis.meister87@gmail.com>
2023-11-12 19:26:26 +0100

    update version

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -16,7 +16,7 @@ export default function Viewer({
     remoteAppPort: remoteAppPort,
     auth: {
       identifier: "de.tealfire.obs",
-      version: "2.0.7",
+      version: "2.0.8",
       name: "TS5 OBS Overlay",
       description: "A OBS overlay for TS5 by DerTyp7",
     },
